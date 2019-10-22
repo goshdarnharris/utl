752ecda74fde1ba65182498fc90bed298d2826ed
George Harris <george@luminairecoffee.com>
2019-10-21 18:18:37 -0700

    algorithm header w/ basic utl::max

---
+++ include/algorithm.hh
@@ -0,0 +1,13 @@
+#ifndef UTL_ALGORITHM_HH_
+#define UTL_ALGORITHM_HH_
+
+namespace utl {
+
+template <typename T>
+constexpr T const& max(T const& a, T const& b) {
+    return a > b ? a : b;
+}
+
+} //namespace utl
+
+#endif //UTL_ALGORITHM_HH_
